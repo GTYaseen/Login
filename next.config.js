ae729ba7981b7ea10e2295d1742d882b0501f10a
GTYaseen <Xxyaseent11xX14@gmail.com>
2023-12-10 23:37:02 +0300

    still

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["cdn.discordapp.com"],
+    },
+}
 
 module.exports = nextConfig
--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ const Login = () => {
   const router = useRouter();
   const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/login', { username, password });
+      const response = await axios.post('https://server-yaseen.onrender.com/login', { username, password });
 
       // Handle successful login
       const token = response.data.token;
